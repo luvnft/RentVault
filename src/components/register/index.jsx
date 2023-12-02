c2179076a5f03b48d5fa9283397b50d9c2755a3d
luv nft <info@blkluv.org>
2023-12-02 08:24:41 -0500

    lighthouse

--- src/components/register/index.jsx
+++ src/components/register/index.jsx
@@ -12,7 +12,7 @@ const RegisterProperty = () => {
         // Both file and folder are supported by upload function
         // Third parameter is for multiple files, if multiple files are to be uploaded at once make it true
         // Fourth parameter is the deal parameters, default null
-        const output = await lighthouse.upload(file, "eea578f0.cf6e6b4e2e0345e5847cf880cc3811fd", false, null, progressCallback);
+        const output = await lighthouse.upload(file, "2ee6b82e.9a1e6ab8e87b40c69c5d918f82d3d9cb", false, null, progressCallback);
         console.log('File Status:', output);
         /*
           output:
@@ -28,7 +28,7 @@ const RegisterProperty = () => {
     }
 
     const RetriveUploads = async() =>{
-        const apiKey = 'eea578f0.cf6e6b4e2e0345e5847cf880cc3811fd'; // Make sure to replace with your public key
+        const apiKey = '2ee6b82e.9a1e6ab8e87b40c69c5d918f82d3d9cb'; // Make sure to replace with your public key
         const uploads = await lighthouse.getUploads(apiKey);
 
         // Display the list of uploaded files
